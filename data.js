963478196193f825612c2bceebb0d679878fae9b
mark hoel <mdjhoel@gmail.com>
2023-06-07 15:28:50 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,188 @@
+data = [{
+	"name": 'Crosswalk at Bar road and Mowat Drive',
+	"desc": "Bar road and Mowat Drive are connector streets with generous shoulders.",
+	"coordinates": [45.089141609616085, -67.06505298614502],
+	"photo": "xwalk",
+	"rating": "0"
+}, {
+	"name": 'Towards Ministers Island',
+	"desc": "Turn right off the paved trail and head down the hill to the bar. At low tide you can cross.",
+	"coordinates": [45.09433377593718, -67.0587819814682],
+	"photo": "bareast",
+	"rating": "0"
+}, {
+	"name": 'Cornelia and Mowat',
+	"desc": "This is the busiest traffic intersection of the trail. Look both ways!",
+	"coordinates": [45.0913268458189, -67.06638872623444],
+	"photo": "cornelia",
+	"rating": "0"
+}, {
+	"name": 'Bar road entrance',
+	"desc": "The entrance to the paved section of the Van Horne trail. Don't feed the deer!",
+	"coordinates": [45.09428, -67.05881],
+	"photo": "vanhorne",
+	"rating": "0"
+}, {
+	"name": 'Rest stop',
+	"desc": "Relax and chill on the bench. Enjoy the peace and quiet of this location.",
+	"coordinates": [45.090187109010024, -67.05422423381893],
+	"photo": "bench",
+	"rating": "0"
+}, {
+	"name": 'Van Horne trail station',
+	"desc": "Bike tools, plaque to read and a dry place in case of rain.",
+	"coordinates": [45.06989856016825, -67.04375870160882],
+	"photo": "station",
+	"rating": "0"
+}, {
+	"name": 'Accessible bird watching',
+	"desc": "This platform is a great place to view birds and has an accessibility ramp.",
+	"coordinates": [45.07219730803573, -67.041189277033],
+	"photo": "birdwatch",
+	"rating": "0"
+}, {
+	"name": 'Ministers island view',
+	"desc": "At high tide, you can view Ministers island and read the plaque. Come back at low tide to walk or drive across.",
+	"coordinates": [45.099390536466906, -67.05439231172008],
+	"photo": "ministers",
+	"rating": "0"
+}, {
+	"name": 'Passamaquoddy Bay',
+	"desc": "Ride along a coastal beach and wetland with an amazing view of the Bay.",
+	"coordinates": [45.078089094495446, -67.0420167114326],
+	"photo": "bay",
+	"rating": "0"
+}, {
+	"name": 'View from the wharf',
+	"desc": "Check out the boats, sea creatures or the view from the wharf.",
+	"coordinates": [45.07181612943909, -67.05443083399905],
+	"photo": "wharf",
+	"rating": "0"
+}, {
+	"name": 'Aerial view',
+	"desc": "Check out the waterfront, Water street, the Algonquin Hotel, the town and Chamcook Lake.",
+	"coordinates": [45.0707298117314, -67.05240688700147],
+	"photo": "water",
+	"rating": "0"
+}, {
+	"name": 'Historic Water street',
+	"desc": "Slow down and enjoy the sites and sounds of our main street. ",
+	"coordinates": [45.07470161133389, -67.05516904592514],
+	"photo": "streetscape",
+	"rating": "0"
+}, {
+	"name": 'Cornelia / 127 intersection',
+	"desc": "Looking south from Cornelia, a calm residential road, to Marine Science drive. Be careful crossing the 127 here.",
+	"coordinates": [45.08928881005657, -67.07134341391287],
+	"photo": "crn127",
+	"rating": "0"
+}, {
+	"name": 'Marine Science intersection',
+	"desc": "Looking south from Marine Science Drive and 127. This is a road with little traffic. Be careful crossing the 127 here.",
+	"coordinates": [45.08884100385681, -67.07108145330186],
+	"photo": "msd127",
+	"rating": "0"
+}, {
+	"name": 'Brandy Cove crosswalk',
+	"desc": "Brandy Cove road connects Marine Science to Jos Point and runs through the Algonquin golf course.",
+	"coordinates": [45.085508634603755, -67.07637255800212],
+	"photo": "xwalkalgo",
+	"rating": "0"
+}, {
+	"name": 'Algonquin golf course',
+	"desc": "This section is one of the hilliest parts of the trail. Watch out for errant golf balls!",
+	"coordinates": [45.08391354812852, -67.07518747683325],
+	"photo": "golftrail",
+	"rating": "0"
+}, {
+	"name": 'Golf course connector',
+	"desc": "Looking north through the Algonquin golf course. Walk up the hill through the golf course to access Marine Science Drive.",
+	"coordinates": [45.08154082634411, -67.07207472766693],
+	"photo": "brandy",
+	"rating": "0"
+}, {
+	"name": 'Pottery Cove at high tide',
+	"desc": "Looking west over the St. Croix river towards the US state of Maine.",
+	"coordinates": [45.07883765479856, -67.06984736333041],
+	"photo": "pottery",
+	"rating": "0"
+}, {
+	"name": 'Blockhouse National Historic site',
+	"desc": "This fort was built to protect Saint Andrews from invasion from the USA during 1812. It worked!",
+	"coordinates": [45.07740589994959, -67.06184158332547],
+	"photo": "nhs",
+	"rating": "0"
+}, {
+	"name": 'Single track',
+	"desc": "Looking south on a scenic but narrow path linking Katys Cove to playing fields trail section.",
+	"coordinates": [45.0852,-67.0543],
+	"photo": "path",
+	"rating": "0"
+}, {
+	"name": 'Playing fields section',
+	"desc": "Cross Cemetary Road to the paved trail to a look at the town sports fields and continue on the trail. Look both ways!",
+	"coordinates": [45.0854,-67.0543],
+	"photo": "newpath",
+	"rating": "0"
+}, {
+	"name": 'One of Canadas great streets',
+	"desc": "In 2016, Water street was recognized by the Canadian Institute of Planners as one of Canadas great streets. We agree!",
+	"coordinates": [45.073659832237354, -67.05292884081781],
+	"photo": "plaque",
+	"rating": "0"
+}, {
+	"name": 'Irish monument',
+	"desc": "Thousands of Irish immigrants came through St Andrews to seek a better life. Unfortunately, many died on Hospital Island, the quarentine location. The Celtic cross on the waterfront commemorates them.",
+	"coordinates": [45.072296109239424, -67.03759871706848],
+	"photo": "celtic",
+	"rating": "0"
+}, {
+	"name": 'Pagan point trail section',
+	"desc": "This paved section of the trail takes you along the Pagan Point Nature Preserve, providing beautiful views of the coastal marsh and Passamaquoddy Bay.",
+	"coordinates": [45.07456619556769, -67.04047504361094],
+	"photo": "entry",
+	"rating": "0"
+}, {
+	"name": 'Katys Cove beach and playground (and washrooms)',
+	"desc": "Stop and relax at a picnic table or take a dip in the warm protected salt water of Katys Cove.",
+	"coordinates": [45.08316691979863, -67.05136000935501],
+	"photo": "washrooms",
+	"rating": "0"
+}, {
+	"name": 'Old railway trestle (Ministers island lookout)',
+	"desc": "Looking north, you can see the impressive view of Ministers island on your right. Katys Cove is on the left.",
+	"coordinates": [45.08456135305303, -67.04536386814289],
+	"photo": "trestle",
+	"rating": "0"
+},{
+	"name": 'Keep left for loop, right for trestle',
+	"desc": "Traveling north (from town) the trail forks. Turn left to continue on the trail loop, right to go to the (dead end old railway trestle) Ministers Island lookout.",
+	"coordinates": [45.08067845815867, -67.0436936572087],
+	"photo": "left",
+	"rating": "0"
+},{
+	"name": 'Indian point beach',
+	"desc": "Excellent rocky beach, great for those who enjoy searching for unique rocks or shells & sea glass. Picnic tables and green space as well.",
+	"coordinates": [45.07321870562282, -67.03792257290341],
+	"photo": "indian",
+	"rating": "0"
+}, {
+	"name": 'Passamaquoddy view',
+	"desc": "The view looking south over the Bay. Take a moment to enjoy one of best views on the trail.",
+	"coordinates": [45.08320461514308, -67.07460794601343],
+	"photo": "golfview",
+	"rating": "0"
+},{
+	"name": 'Sports field path',
+	"desc": "Looking towards the sports fields down the paved trail added to the Van Horne trail. This section cuts through the playing fields.",
+	"coordinates": [45.08801298001523,-67.05304741859436],
+	"photo": "fields",
+	"rating": "0"
+},{
+	"name": 'Tools on Water street',
+	"desc": "Need to repair your bike?. Use our free tools at Water and William streets!",
+	"coordinates": [45.07432751868502, -67.05436170101164],
+	"photo": "tools",
+	"rating": "0"
+}
+]
